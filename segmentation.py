bdc279b741a21c5fcc553507bfc80b3a6383abd2
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2023-10-10 09:42:01 +0200

    created function for mapping equally spaced seeds onto an image

--- segmentation.py
+++ segmentation.py
@@ -16,8 +16,27 @@ def elevation_map(rgb_im):
     return elevation
 
 
+def map_grid(n_points, shape):
+    """ Creates point grid within a certain shape
+
+    :param n_points: int, number of points that the grid should contain
+    :param shape: iterable, an iterable containing the rows (int) and
+        columns (int)
+    :return np.ndarray, a numpy array with the specified shape, containing an
+        equally spaced grid with the specified number of points
+    """
+    grid = util.regular_grid(shape, n_points)
+    grid_map = np.zeros(shape)
+    print(image.shape)
+    print(grid_map.shape)
+    grid_map[grid] = np.arange(
+        grid_map[grid].size).reshape(grid_map[grid].shape) + 1
+    return grid_map
+
+
 if __name__ == "__main__":
     image = io.imread("test_images/tb_snap.png")
-    elevation = elevation_map(image)
-    plt.imshow(elevation)
+    el_map = elevation_map(image)
+    seeds = map_grid(1000, image.shape[0:2])
+    plt.imshow(seeds)
     plt.show()
