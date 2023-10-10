b296dab036472776e2e417afc1338f505cc72a31
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2023-10-10 19:56:08 +0200

    renamed file

---
+++ bg_segmentation.py
@@ -0,0 +1,71 @@
+"""
+Author: Chris Dijkstra
+Date: 10/10/2023
+
+Contains functions to run segmentation from the command line
+"""
+import argparse as arg
+import os
+import segment
+import matplotlib.pyplot as plt
+from skimage import io
+
+
+def arg_reader():
+    """ Reads arguments from command line
+
+    :return ..., class containing the arguments
+    """
+    arg_parser = arg.ArgumentParser(
+        description="Segments rgb images based on predetermined thresholds"
+    )
+    arg_parser.add_argument("filename", help="The path to the directory holding"
+                                             " the images")
+    arg_parser.add_argument(
+        "seeds",
+        help="The number of starting seeds for the watershed segmentation",
+        type=int
+    )
+    arg_parser.add_argument(
+        "-ht",
+        help="The hue threshold, every pixel where the hue is below this will "
+             "be marked as 0. Hue is given in a range between 0 and 1.",
+        type=float, default=0.0
+    )
+    arg_parser.add_argument(
+        "-st",
+        help="The saturation threshold, every pixel where the saturation is "
+             "below this will be marked as 0. Saturation is given in a range "
+             "between 0 and 1.",
+        type=float, default=0.0
+    )
+    arg_parser.add_argument(
+        "-vt",
+        help="the value threshold, every pixel where the value will be below"
+             "this threshold will be marked as 0. Saturation is given between 0"
+             "and 1",
+        type=float, default=0.0
+    )
+    return arg_parser.parse_args()
+
+
+def main():
+    args = arg_reader()
+    directory = args.filename
+    if os.path.isdir(directory):
+        files = os.listdir(directory)
+        for file in files:
+            try:
+                image = io.imread(directory + "/" + file)
+            except:
+                print(f"Could not open {directory + '/' + file}")
+            else:
+                mask = segment.water_hsv_thresh(image, args.seeds, args.ht,
+                                                args.vt, args.st)
+                io.imsave(mask, f"out/{file}", arr=image.shape[0:2])
+                plt.imshow(mask)
+                plt.show()
+
+
+if __name__ == "__main__":
+    main()
