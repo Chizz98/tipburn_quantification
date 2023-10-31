54d9992aeaafa799a8f721e1959d72f0b25cafeb
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2023-10-31 17:02:42 +0100

    Script for parsing segmentations separately

---
+++ scripts/parse_segmentations.py
@@ -0,0 +1,29 @@
+import tipburn_segmentation
+import argparse as arg
+import os
+
+
+def arg_reader():
+    """ Reads arguments from command line
+
+    :return ..., class containing the arguments
+    """
+    arg_parser = arg.ArgumentParser(
+        description="Segments rgb images based on predetermined thresholds"
+    )
+    arg_parser.add_argument("filename", help="The path to the directory holding"
+                                             " the images")
+    arg_parser.add_argument("out", help="The directory to write the pixel table"
+                                        "to. Does not need to be "
+                                        "pre_existing.")
+    return arg_parser.parse_args()
+
+
+def main():
+    args = arg_reader()
+    files = [args.filename + "/" + file for file in os.listdir(args.filename)]
+    tipburn_segmentation.parse_segmentations(files, args.out)
+
+
+if __name__ == "__main__":
+    main()
