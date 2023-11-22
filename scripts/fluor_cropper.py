c71c30447682c25b063eef60b05e47f8ed237a26
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2023-11-22 08:31:22 +0100

    Reworked it to take fluor files first instead of RGB, lessens redundant file opening

--- scripts/fluor_cropper.py
+++ scripts/fluor_cropper.py
@@ -54,7 +54,9 @@ def rough_crop(mask_a, mask_b, step):
     return optimum
 
 
-def overlap_crop(mask_a, mask_b, full_image, return_coords=False):
+def overlap_crop(rgb_im, full_image):
+    mask_a = segment.shw_segmentation(rgb_im)
+    mask_b = full_image > filters.threshold_otsu(full_image)
     rough_cen = rough_crop(mask_a, mask_b, 50)
     crop = utils.crop_region(full_image, rough_cen, (1500, 1500))
     crop_mask = crop > filters.threshold_otsu(crop)
@@ -66,75 +68,45 @@ def overlap_crop(mask_a, mask_b, full_image, return_coords=False):
         reference_image=rgb_mask,
         moving_image=crop_mask
     )
-    full_image = np.pad(full_image, 500)
-    final_crop = utils.crop_region(
-        full_image,
-        (int(rough_cen[0] - shift[1] + 500),
-         int(rough_cen[1] - shift[0] + 500)),
-        (1500, 1500)
-    )
-    if return_coords:
-        return(final_crop, (int(rough_cen[0] - shift[1]),
-                            int(rough_cen[1] - shift[0])))
-    return final_crop
+    return (int(rough_cen[0] - shift[1]), int(rough_cen[1] - shift[0]))
+
+
+def worker(arg_tup):
+    fm, rgbs, fvfm, cmd_args = arg_tup
+    try:
+        fm_im = transform.resize(utils.read_fimg(fm), (2823, 3750))
+        fvfm_im = transform.resize(utils.read_fimg(fvfm), (2823, 3750))
+    except Exception as e:
+        print(f"could not read fluorescence images, "
+              f"Exception: {e}")
+    for rgb in rgbs:
+        rgb_im = io.imread(rgb)
+        rgb_fn = rgb.split("/")[-1]
+        new_centre = overlap_crop(rgb_im, fm_im)
+        fm_crop = utils.crop_region(np.pad(fm_im, 500),
+                                    (new_centre[0] + 500,
+                                     new_centre[1] + 500),
+                                    (1500, 1500))
+        fvfm_crop = utils.crop_region(np.pad(fvfm_im, 500),
+                                      (new_centre[0] + 500,
+                                       new_centre[1] + 500),
+                                      (1500, 1500))
+        np.save(cmd_args.out + "/" + rgb_fn.replace(".png", "_Fm"), fm_crop)
+        np.save(cmd_args.out + "/" + rgb_fn.replace(".png", "_FvFm"), fvfm_crop)
+        if cmd_args.d:
+            if not os.path.isdir(cmd_args.out + "/diagnostic"):
+                os.mkdir(cmd_args.out + "/diagnostic")
+            plt.imsave(cmd_args.out + "/diagnostic/" + rgb_fn.replace(
+                ".png", "_Fm.png"),
+                       fm_crop)
+            plt.imsave(cmd_args.out + "/diagnostic/" + rgb_fn.replace(
+                ".png", "_FvFm.png"),
+                       fvfm_crop)
 
 
 def pool_handler(cores, fun, params):
     pools = Pool(cores)
     pools.map(fun, params)
-
-
-def worker(arg_tup):
-    print(f"Starting work on {arg_tup}")
-    rgb_crop, fluor_dir, outdir, diag = arg_tup
-    rgb_fn = rgb_crop.split("/")[-1]
-    ident = "-".join(rgb_fn.split("-")[:3])
-    fluor_files = os.listdir(fluor_dir)
-    try:
-        fm_match = [file for file in fluor_files if
-                    file.startswith(ident) and file.endswith("-Fm.fimg")][0]
-        fvfm_match = [file for file in fluor_files if
-                      file.startswith(ident) and file.endswith("-Fv_Fm.fimg")][0]
-    except Exception as e:
-        print(f"No matching fluor files found for {rgb_crop}."
-              f"Exception: {e}")
-        return
-    try:
-        rgb_im = io.imread(rgb_crop)
-    except Exception as e:
-        print(f"Rgb file {rgb_crop} could not be read."
-              f"Exception: {e}")
-        return
-    try:
-        fm_im = utils.read_fimg(fluor_dir + "/" + fm_match)
-        fvfm_im = utils.read_fimg(fluor_dir + "/" + fvfm_match)
-    except Exception as e:
-        print(f"Fluorescence files {fluor_dir + '/' + fm_match}"
-              f"{fluor_dir + '/' + fvfm_match}) could not be read."
-              f"Exception: {e}")
-        return
-    fm_im = transform.resize(fm_im, (2823, 3750))
-    fvfm_im = transform.resize(fvfm_im, (2823, 3750))
-    rgb_mask = segment.shw_segmentation(rgb_im)
-    fm_mask = fm_im > filters.threshold_otsu(fm_im)
-    fm_crop, centre = overlap_crop(rgb_mask, fm_mask, fm_im, True)
-    fvfm_im = np.pad(fvfm_im, 500)
-    fvfm_crop = utils.crop_region(
-        fvfm_im,
-        (centre[0] + 500, centre[1] + 500),
-        shape=(1500, 1500)
-    )
-    np.save(outdir + "/" + rgb_fn.replace(".png", "_Fm"), fm_crop)
-    np.save(outdir + "/" + rgb_fn.replace(".png", "_FvFm"), fvfm_crop)
-    if diag:
-        if not os.path.isdir(outdir + "/diagnostic"):
-            os.mkdir(outdir + "/diagnostic")
-        plt.imsave(outdir + "/diagnostic/" + rgb_fn.replace(
-            ".png", "_Fm.png"),
-                   fm_crop)
-        plt.imsave(outdir + "/diagnostic/" + rgb_fn.replace(
-            ".png", "_FvFm.png"),
-                   fvfm_crop)
 
 
 def main():
@@ -144,13 +116,22 @@ def main():
     if not os.path.isdir(args.out):
         os.mkdir(args.out)
     # Create list of files
-    files = [args.rgb_path + "/" + file for file in os.listdir(args.rgb_path)]
-    # Create list of parameters
-    params = zip(files, [args.fluor_path] * len(files), [args.out] * len(files),
-                 [args.d] * len(files))
-    # Send to pool handler
-    pool_handler(args.c, worker, params)
-
+    fluor_files = [args.fluor_path + "/" + file for file in
+                   os.listdir(args.fluor_path) if file.endswith("-Fm.fimg")]
+    rgb_files = [args.rgb_path + "/" + file for file in
+                 os.listdir(args.rgb_path)]
+    # Match dict
+    match_dict = {}
+    for file in fluor_files:
+        ident = "-".join(file.split("/")[-1].split("-")[0:3])
+        match_dict[file] = [file for file in rgb_files if
+                            file.split("/")[-1].startswith(ident)]
+    params = [tuple(list(tup) +
+                    [tup[0].replace("-Fm.fimg", "-Fv_Fm.fimg"),
+                     args]) for tup in match_dict.items()]
+    pool_handler(args.c,
+                 worker,
+                 params)
 
 
 if __name__ == "__main__":
