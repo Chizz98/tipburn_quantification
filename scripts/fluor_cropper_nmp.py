0f70ccf1a646d83a3f85356ba44e5320d9a4d505
Chizz98 <116356082+Chizz98@users.noreply.github.com>
2023-11-27 20:42:17 +0100

    Added logger name to log entries

--- scripts/fluor_cropper_nmp.py
+++ scripts/fluor_cropper_nmp.py
@@ -125,7 +125,7 @@ def main():
         filename="fluor_cropper.log",
         filemode="w",
         level=logging.DEBUG,
-        format="%(asctime)s;%(levelname)s;%(message)s",
+        format="%(asctime)s;%(name)s;%(levelname)s;%(message)s",
         datefmt="%Y-%m-%d %H:%M:%S"
     )
     logger = logging.getLogger()
